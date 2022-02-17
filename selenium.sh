80e6efbcb85aaa40981436e1ddbe2e591b8a44ed
Mike <johndoe@example.com>
2022-02-17 18:01:12 -0500

    d

--- selenium.sh
+++ selenium.sh
@@ -1,2 +1,19 @@
+# Install venv
+
+sudo apt install python3.8-venv &&
+
+# Install selenium
+
 pip install selenium && 
 
+# Install kijiji-manager
+
+git clone https://github.com/jackm/kijiji-manager.git &&
+cd kijiji-manager &&
+
+python3 -m venv venv &&
+source venv/bin/activate &&
+
+pip install . &&
+
+cd kijiji_manager &&
