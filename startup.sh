5f69fe6daa71df7fec8f722366cccefa36af1e20
Mike <johndoe@example.com>
2022-02-19 12:23:22 -0500

    d

--- startup.sh
+++ startup.sh
@@ -1,39 +1,33 @@
 #!/bin/bash
 
-sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y &&
+# Update
+sudo apt update &&
+sudo apt upgrade -y &&
+sudo apt autoremove -y &&
 
-sudo apt install git -y &&  sudo apt install python3-pip -y &&
+# Install Packages
+sudo apt install git -y &&
+sudo apt install python3-pip -y &&
+sudo apt-get install python3-venv -y &&
+pip3 install wheel &&
+pip3 install selenium &&
+pip3 install -r ~/startup/requirements.txt &&
 
 # Clone Repository
-
 git clone https://github.com/michaelgambino/startup.git &&
 
-# Install venv
-
-sudo apt-get install python3-venv -y &&
-
-# Install selenium
-
-pip3 install selenium && pip3 install -r ~/startup/requirements.txt && pip3 install wheel &&
-
 # Install kijiji-manager
-
 git clone https://github.com/jackm/kijiji-manager.git &&
 cd kijiji-manager && mkdir instance &&
-
 python3 -m venv venv &&
 source venv/bin/activate &&
-
 pip3 install . &&
-
 cd kijiji_manager &&
-
 python3 ~/Desktop/startup/keychange.py &&
-
 mv kijiji-manager.cfg ~/kijiji-manager/instance/kijiji-manager.cfg &&
 
+# Repost
 cd && cd kijiji-manager &&
-
+source venv/bin/activate &&
 python3 -m kijiji_manager &&
-
-python3 ~/startup/main.py
+python3 ~/startup/repost.py
