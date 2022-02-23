dd005c5427d566c4c0102a44f773cbf8bdce139e
Mike <johndoe@example.com>
2022-02-22 23:30:05 -0500

    d

--- archinstall.sh
+++ archinstall.sh
@@ -7,8 +7,21 @@ echo $0
 # connect to network
 iwctl
 station "device" connect "SSID"
+# test connection
+ping google.com
 
 # update pacman
 sudo pacman -Syu --noconfirm
 # update keyring
 sudo pacman -S archlinux-keyring
+
+# update the system clock
+timedatectl set-ntp true
+
+fdisk /dev/sda
+
+g 
+n
+
+
+
--- dd.sh
+++ dd.sh
@@ -1,7 +1,20 @@
-# list partitione
+# list partitions
 parted -l
 # zero fill drive, time displays time taken after
 time dd if=/dev/zero of="drive" bs=1024 status=progress
 
 # random fill drive
 dd if=/dev/urandom of=/dev/sda
+
+
+# BOOTABLE USB
+
+# get name of drive
+lsblk -l
+# unmount drive
+umount /dev/sdb*
+# format drive
+mkfs.ext4 /dev/sdb
+# create the bootable usb
+time dd if=/home/mike/Desktop/files/iso/archlinux-2021.10.01-x86_64.iso of=/dev/sdb bs=1M status=progress
+
